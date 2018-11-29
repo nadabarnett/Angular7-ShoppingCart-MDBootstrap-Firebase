b3c40aba78fabc0d9396d523b0aeb1705db841ba
topdeveloper2019 <topdeveloper2019@outlook.com>
2018-11-29 21:39:36 +0800

    initial commit

---
+++ src/app/index/navbar/navbar.component.ts
@@ -0,0 +1,41 @@
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  VERSION
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../../shared/services/auth.service";
+import { ProductService } from "../../shared/services/product.service";
+import { TranslateService } from "../../shared/services/translate.service";
+declare var $: any;
+
+@Component({
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
+})
+export class NavbarComponent implements OnInit {
+  angularVersion = VERSION;
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    public productService: ProductService,
+    public translate: TranslateService
+  ) {
+    // console.log(translate.data);
+  }
+
+  ngOnInit() {}
+  logout() {
+    this.authService.logout();
+    this.router.navigate(["/"]);
+  }
+
+  setLang(lang: string) {
+    // console.log("Language", lang);
+    this.translate.use(lang).then(() => {});
+  }
+}
